e020af8354a25280c47d89f6d2a4f303755dba9f
Amine Kamal <m.amine.kamal@gmail.com>
2019-02-15 14:34:21 -0500

    Added weka

---
+++ TP1/weka-3.8/weka/src/test/java/weka/AllTestsNeeded.java
@@ -0,0 +1,26 @@
+package weka;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTestsNeeded extends TestSuite{
+	public static Test suite() {
+	    TestSuite suite = new TestSuite();
+
+	    suite.addTest(weka.associations.FPGrowthTest.suite());
+	    
+	    suite.addTest(weka.clusterers.FarthestFirstTest.suite());
+	    
+	    suite.addTest(weka.clusterers.SimpleKMeansTest.suite());
+	    
+	    suite.addTest(weka.classifiers.bayes.NaiveBayesTest.suite());
+	    
+	    suite.addTest(weka.classifiers.trees.DecisionStumpTest.suite());
+
+	    return suite;
+	  }
+
+	  public static void main(String[] args) {
+	    junit.textui.TestRunner.run(suite());
+	  }
+}
